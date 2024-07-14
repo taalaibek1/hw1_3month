77f13b81669bc0c2fe46d6b3eebc23f8e36fdaf0
taalaibek1 <islamov.dan1yar777@gmail.com>
2024-07-14 14:14:45 +0600

    Initial commit

---
+++ app/src/main/java/com/example/hw1_3month/MainActivity.java
@@ -0,0 +1,47 @@
+package com.example.hw1_3month;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+
+    public void onClickSend(View view) {
+
+        EditText emailEditText = findViewById(R.id.ed_email);
+        EditText themeEditText = findViewById(R.id.ed_theme);
+        EditText messageEditText = findViewById(R.id.ed_message);
+
+        String email = emailEditText.getText().toString();
+        String theme = themeEditText.getText().toString();
+        String message = messageEditText.getText().toString();
+
+
+        Intent intent = new Intent(Intent.ACTION_SENDTO);
+        intent.setData(Uri.parse("mailto:"));
+        intent.putExtra(Intent.EXTRA_EMAIL, new String[]{email});
+        intent.putExtra(Intent.EXTRA_SUBJECT, theme);
+        intent.putExtra(Intent.EXTRA_TEXT, message);
+        startActivity(intent);
+    }
+}
